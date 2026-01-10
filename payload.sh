fbe7f9195f46090eff0bb6a377188300160da9fb
Luke Macken <luke@phorex.org>
2026-01-09 17:23:19 -0700

    Bump version to 4.0

--- payload.sh
+++ payload.sh
@@ -2,7 +2,7 @@
 # Title: DOOM
 # Description: The classic 1993 FPS on your WiFi Pineapple Pager!
 # Author: @lmacken
-# Version: 2.0
+# Version: 4.0
 # Category: Games
 
 PAYLOAD_DIR="/root/payloads/user/games/doom"
